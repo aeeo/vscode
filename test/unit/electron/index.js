430659db4e402a3bafa653ff14bef022ebf3956a
Victor(Tong Zhao) <zhaotong@suunto.cn>
2025-10-14 15:57:30 +0800

    Rename Code - OSS to Suunto JS throughout project

--- test/unit/electron/index.js
+++ test/unit/electron/index.js
@@ -240,11 +240,11 @@ app.on('ready', () => {
 		return {
 			product: {
 				version: '1.x.y',
-				nameShort: 'Code - OSS Dev',
-				nameLong: 'Code - OSS Dev',
-				applicationName: 'code-oss',
+				nameShort: 'Suunto JS Dev',
+				nameLong: 'Suunto JS Dev',
+				applicationName: 'suunto-js',
 				dataFolderName: '.vscode-oss',
-				urlProtocol: 'code-oss',
+				urlProtocol: 'suunto-js',
 			}
 		};
 	});
