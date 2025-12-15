28767b474333bb56a8d56e5d1fc3421a7f83839d
Victor(Tong Zhao) <zhaotong@suunto.cn>
2025-12-15 17:03:51 +0800

    Add action openSimulator
    Change-Id: I9a7995f15c8d2c7daaa998b22b8f76c007827fcb

--- src/vs/workbench/contrib/suuntoJS/browser/suuntoJS.contribution.ts
+++ src/vs/workbench/contrib/suuntoJS/browser/suuntoJS.contribution.ts
@@ -12,22 +12,33 @@ import { Action2, MenuId, MenuRegistry, registerAction2 } from '../../../../plat
 import { ServicesAccessor } from '../../../../platform/instantiation/common/instantiation.js';
 import { ISuuntoJSService } from '../../../services/suuntoJS/common/suuntoJS.js';
 
+const SUUNTO_JS_ACTION_ID = 'suuntoJS.action';
+
 class SuuntoJSAction extends Action2 {
 	constructor() {
 		super({
-			id: 'suuntoJS.action1',
-			title: localize2('suuntoJS.action1', "SuuntoJS Action 1"),
-			icon: Codicon.settings,
+			id: SUUNTO_JS_ACTION_ID,
+			title: localize2('suuntoJS.action', "SuuntoJS Action"),
 			f1: false
 		});
 	}
 
-	run(accessor: ServicesAccessor) {
+	run(accessor: ServicesAccessor, ...args: unknown[]) {
 		const suuntoJSService = accessor.get(ISuuntoJSService);
 
 		suuntoJSService.showMessage('SuuntoJS Action 1 executed');
-
 		console.log('SuuntoJS Action 1 executed');
+
+		const actionType = args[0];
+		switch (actionType) {
+			case 'openSimulator':
+				// suuntoJSService.openSimulator();
+				break;
+			case 'action2':
+				break;
+			default:
+				break;
+		}
 	}
 }
 
@@ -35,11 +46,21 @@ registerAction2(SuuntoJSAction);
 
 MenuRegistry.appendMenuItem(MenuId.SuuntoJSContext, {
 	group: '2_configuration',
+	order: 0,
+	command: {
+		id: SUUNTO_JS_ACTION_ID,
+		title: localize('suuntoJS.action.openSimulator', "Open SuuntoJS Simulator"),
+		// arguments: ['openSimulator']
+	},
+	when: undefined
+});
+
+MenuRegistry.appendMenuItem(MenuId.SuuntoJSContext, {
+	group: '2_configuration',
 	order: 1,
 	command: {
-		id: 'suuntoJS.action1',
-		title: localize('suuntoJS.action1', "SuuntoJS Action 1"),
-		icon: Codicon.settings
+		id: SUUNTO_JS_ACTION_ID,
+		title: localize('suuntoJS.action.action2', "Action 2"),
 	},
 	when: undefined
 });
